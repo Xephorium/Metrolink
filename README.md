564215d700552731565e2d34f4b49fcd0c1548a2
Xephorium <christopher.a.cruzen@gmail.com>
2015-05-06 22:38:45 -0500

    Initial Code Upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Metrolink
+LaunchCode Java CS50 Problem Set #6
+
+Directory Notes:
+- 'Metrolink.java' (contains main) can be found in the com.xephorium.Metrolink package within src\main\java as per Maven directory tree specifications.
---
+++ src/main/java/com/xephorium/metrolink/Metrolink.java
@@ -0,0 +1,30 @@
+/*
+  Christopher Cruzen                         LaunchCode CS50
+  "Metrolink.java"                                05.06.2015
+
+  Problem Set #6
+
+    Create a command line application that employs SQLite to
+  read data from a simple database. The application should
+  list all metrolink stations, ask the user which station
+  they're currently at, and display the amount of time until
+  that station's next train will arrive.
+    Be sure the project uses maven, Spring, and is unit
+  testable.
+
+*/
+package com.xephorium.metrolink;
+
+public class Metrolink
+{
+    /*--- Fields ---*/
+
+    /*--- Constructors ---*/
+
+    /*--- Methods ---*/
+
+    public static void main(String[] args)
+    {
+        // TBD
+    }
+}
